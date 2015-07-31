86aebbb8214425394d546c0cd2bb6f210e83ed37
vagrant <vagrant@vagrant.vm>
2015-07-31 09:08:21 +0000

    fix

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -v "$PWD/esdata":/usr/share/elasticsearch/data zhongpei/elasticsearch
+docker run --name elasticsearch  -d -v "$PWD/esdata":/usr/share/elasticsearch/data zhongpei/elasticsearch
